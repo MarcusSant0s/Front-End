6fa2c799d3effd25ab54481b8549e9da38a8b347
MarcusSant0s <marcus.pereira.paulo@gmail.com>
2024-11-13 15:36:24 -0300

    Upgrade Form - added  'informação profissional'

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import InputMask from "react-input-mask"; // Importing InputMask for phone and document fields
+import InputMask from "react-input-mask"; // Importando o InputMask para campos de telefone e documento
 
 export default function Form() {
-  // Form state
+  // Estado do formulário
   const [values, setValues] = useState({
     name: "",
     lastName: "",
@@ -11,9 +11,12 @@ export default function Form() {
     numberDocument: "",
     gender: "",
     phoneNumber: "",
+    contractType: "",  // Novo campo para formato de contratação
+    isActive: false,   // Novo campo para indicar se está ativo (booleano)
+    observation: "",   // Novo campo de observação
   });
 
-  // Error state
+  // Estado de erros
   const [errors, setErrors] = useState({
     name: "",
     lastName: "",
@@ -22,13 +25,29 @@ export default function Form() {
     numberDocument: "",
     phoneNumber: "",
     gender: "",
+    contractType: "",
+    isActive: "",
+    observation: "",
   });
 
-  // Handle field changes
+  // Função para lidar com as mudanças nos campos
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
 
-    // Basic field validation: No numbers in name or last name
+    // Se for checkbox, atualiza o valor como booleano
+    if (type === "checkbox") {
+      setValues({
+        ...values,
+        [name]: checked,
+      });
+    } else {
+      setValues({
+        ...values,
+        [name]: value,
+      });
+    }
+
+    // Validação básica para não permitir números nos campos de nome e sobrenome
     if (name === "name" || name === "lastName") {
       if (/[\d]/.test(value)) {
         setErrors({
@@ -42,19 +61,19 @@ export default function Form() {
         });
       }
     }
-
-    setValues({
-      ...values,
-      [name]: value,
-    });
   };
 
-  // Age validation: between 1 and 100
+  // Validação da idade (deve ser maior ou igual a 18 anos para contratação)
   const validateAge = (age) => {
-    if (age < 1 || age > 100) {
+    if (age < 18) {
       setErrors({
         ...errors,
-        age: "A idade deve ser um número de entre 1 e 100.",
+        age: "A idade mínima para contratação é 18 anos.",
+      });
+    } else if (age < 1 || age > 100) {
+      setErrors({
+        ...errors,
+        age: "A idade deve ser um número entre 1 e 100.",
       });
     } else {
       setErrors({
@@ -64,13 +83,13 @@ export default function Form() {
     }
   };
 
-  // Form submit handler
+  // Função de envio do formulário
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
     let formIsValid = true;
 
-    // Reset errors before validation
+    // Resetando os erros antes de validar
     let newErrors = {
       name: "",
       lastName: "",
@@ -79,9 +98,12 @@ export default function Form() {
       numberDocument: "",
       phoneNumber: "",
       gender: "",
+      contractType: "",
+      isActive: "",
+      observation: "",
     };
 
-    // Check for empty fields
+    // Verificando campos obrigatórios
     if (!values.name.trim()) {
       newErrors.name = "O nome é obrigatório.";
       formIsValid = false;
@@ -94,6 +116,9 @@ export default function Form() {
 
     if (!values.age.trim()) {
       newErrors.age = "A idade é obrigatória.";
+      formIsValid = false;
+    } else if (parseInt(values.age) < 18) {
+      newErrors.age = "A idade mínima para contratação é 18 anos.";
       formIsValid = false;
     } else if (parseInt(values.age) < 1 || parseInt(values.age) > 100) {
       newErrors.age = "A idade deve ser entre 1 e 100.";
@@ -120,10 +145,15 @@ export default function Form() {
       formIsValid = false;
     }
 
-    // Update errors if there are any
+    if (!values.contractType) {
+      newErrors.contractType = "Selecione o formato de contratação.";
+      formIsValid = false;
+    }
+
+    // Atualizando os erros se houver algum
     setErrors(newErrors);
 
-    // Only submit if form is valid
+    // Somente enviar o formulário se ele for válido
     if (formIsValid) {
       alert("Formulário enviado com sucesso!");
       // Aqui você pode realizar a lógica de envio para a API ou outro processo
@@ -134,8 +164,9 @@ export default function Form() {
     <div className="max-w-xl mx-auto bg-white p-8 rounded-lg shadow-lg mt-2">
       <h2 className="text-2xl font-semibold text-gray-800 mb-6">Formulário de Cadastro</h2>
 
+      <h4 className="font-semibold text-gray-600 mb-1 border-b border-gray-300">Dados Pessoais</h4>
       <form onSubmit={handleFormSubmit}>
-        {/* Name */}
+        {/* Nome */}
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">
             Primeiro Nome
@@ -152,7 +183,7 @@ export default function Form() {
           {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name}</p>}
         </div>
 
-        {/* Last Name */}
+        {/* Sobrenome */}
         <div className="mb-4">
           <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
             Sobrenome
@@ -169,7 +200,7 @@ export default function Form() {
           {errors.lastName && <p className="text-red-500 text-xs mt-1">{errors.lastName}</p>}
         </div>
 
-        {/* Age */}
+        {/* Idade */}
         <div className="mb-4">
           <label htmlFor="age" className="block text-sm font-medium text-gray-700">
             Idade
@@ -189,7 +220,7 @@ export default function Form() {
           {errors.age && <p className="text-red-500 text-xs mt-1">{errors.age}</p>}
         </div>
 
-        {/* Document Type */}
+        {/* Tipo de Documento */}
         <div className="mb-4">
           <label htmlFor="typeDocument" className="block text-sm font-medium text-gray-700">
             Tipo de Documento
@@ -210,7 +241,7 @@ export default function Form() {
           {errors.typeDocument && <p className="text-red-500 text-xs mt-1">{errors.typeDocument}</p>}
         </div>
 
-        {/* Document Number (CPF / CNPJ) */}
+        {/* Número do Documento (CPF / CNPJ) */}
         <div className="mb-4">
           <label htmlFor="numberDocument" className="block text-sm font-medium text-gray-700">
             Número do Documento
@@ -227,7 +258,7 @@ export default function Form() {
           {errors.numberDocument && <p className="text-red-500 text-xs mt-1">{errors.numberDocument}</p>}
         </div>
 
-        {/* Gender */}
+        {/* Gênero */}
         <div className="mb-4">
           <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
             Gênero
@@ -248,7 +279,7 @@ export default function Form() {
           </select>
         </div>
 
-        {/* Phone Number */}
+        {/* Número de Telefone */}
         <div className="mb-4">
           <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">
             Número de Telefone
@@ -265,7 +296,61 @@ export default function Form() {
           {errors.phoneNumber && <p className="text-red-500 text-xs mt-1">{errors.phoneNumber}</p>}
         </div>
 
-        {/* Submit Button */}
+
+        <h4 className="font-semibold text-gray-600  mt-8 mb-3 border-b border-gray-300">Dados Profissionais</h4>
+        {/* Formato de Contratação */}
+        <div className="mb-4">
+          <label htmlFor="contractType" className="block text-sm font-medium text-gray-700">
+            Formato de Contratação
+          </label>
+          <select
+            id="contractType"
+            name="contractType"
+            value={values.contractType}
+            onChange={handleChange}
+            className="mt-1 py-3 px-4 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-gray-400 focus:outline-none"
+          >
+            <option value="" disabled hidden>
+              Selecione o formato de contratação
+            </option>
+            <option value="PJ">PJ</option>
+            <option value="CLT">CLT</option>
+          </select>
+          {errors.contractType && <p className="text-red-500 text-xs mt-1">{errors.contractType}</p>}
+        </div>
+
+        {/* Ativo */}
+        <div className="mb-4">
+          <label htmlFor="isActive" className="block text-sm font-medium text-gray-700">
+            Contrato Ativo
+          </label>
+          <input
+            type="checkbox"
+            id="isActive"
+            name="isActive"
+            checked={values.isActive}
+            onChange={handleChange}
+            className="mt-1 w-5 h-5 text-gray-900 focus:ring-2 focus:ring-gray-400"
+          />
+        </div>
+
+        {/* Observação */}
+        <div className="mb-4">
+          <label htmlFor="observation" className="block text-sm font-medium text-gray-700">
+            Observação
+          </label>
+          <textarea
+            id="observation"
+            name="observation"
+            value={values.observation}
+            onChange={handleChange}
+            rows="3"
+            className="mt-1 p-3 w-full border border-gray-300 rounded-md focus:ring-2 focus:ring-gray-400 focus:outline-none"
+            placeholder="Digite qualquer observação (opcional)"
+          />
+        </div>
+
+        {/* Botão de Enviar */}
         <div className="mt-6">
           <button
             type="submit"
