6d73eb42a085021b05ee7d9140d85736fc562299
MarcusSant0s <marcus.pereira.paulo@gmail.com>
2024-11-13 14:38:23 -0300

    Create NavBar and SideBar

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function SideBar () {
+
+  return (
+    <aside className='h-full'>
+        <div>
+
+            <ul>
+                <li>Exemplo 1</li>
+                <li>Exemplo 1</li>
+                <li>Exemplo 1</li>
+                <li>Exemplo 1</li>
+            </ul>
+        </div>
+    </aside>
+  )
+}
+
+  
