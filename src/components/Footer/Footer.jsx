3bd037c3c9c13edab91225af5018be84e86e589e
MarcusSant0s <sistemas@cabosat.com.br>
2024-11-18 15:47:46 -0300

    Create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="bg-white mt-96 p-4 text-center border-t">
+        <p>© 2024 Meu Sistema</p>
+    </footer>
+  )
+}
