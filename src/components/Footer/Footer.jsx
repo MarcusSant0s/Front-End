c301b6edcdc24a79e8ddda77d50186ea2b5a71f8
MarcusSant0s <sistemas@cabosat.com.br>
2024-11-21 17:11:41 -0300

    UPGRADE Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="bg-white mt-96 p-4 text-center border-t">
+    <footer className="bg-white  p-4 text-center border-t">
         <p>© 2024 Cabosat</p>
     </footer>
   )
